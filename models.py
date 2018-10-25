9b17653a8b0d1a65ba1a2a3845fd011bbde488c3
Junjie Jin <jjjjohnsonjin@gmail.com>
2018-10-25 15:39:23 +0800

    Add basic model structure

--- models.py
+++ models.py
@@ -20,7 +20,31 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        def conv_bn(inp, oup, stride, kernel_size=3):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True)
+            )
+        def conv_dw(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
+    
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+            )
+        self.model = nn.Sequential(conv_bn(1, 32, 1, 5),
+                           conv_dw( 32,  64, 2),# 112 x 112
+                           conv_dw( 64,  128, 2), # 56 x 56 
+                           conv_dw( 128,  256, 2), # 28 x 28
+                           conv_dw( 256,  512, 2), # 14 x 14
+                           conv_dw( 512,  1024, 2), # 7 x 7
+                           nn.AvgPool2d(7, ceil_mode=True),) 
+                
+        self.fc = nn.Linear(1024, 136)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -32,6 +56,11 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        x = self.model(x)
+        x = x.view(-1, 1024)
+        x = self.fc(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
